ef5b8729b13b175cede42b285873fc1ea7135ab8
@juliabresolin <julia.bresolin@jbq.global>
2024-01-15 10:45:42 -0300

    feat(web): create main section

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
 import { Button } from "./components/ui/button";
 import { Github } from 'lucide-react'
 import { Separator } from "./components/ui/separator";
+import { Textarea } from "./components/ui/textarea";
 
 export function App() {
 
@@ -19,6 +20,20 @@ export function App() {
             </Button>
         </div>
       </div>
+
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
+            <Textarea placeholder="Inclua o prompt para a IA..." className="resize-none p-4 leading-relaxed"/>
+            <Textarea placeholder="Resultado gerado pela IA..." readOnly className="resize-none p-4 leading-relaxed"/>
+          </div>
+
+          <p className="text-sm text-muted-foreground">
+            Lembre-se você pode utilizar a variável <code className="text-primary">{'{transcription}'}</code> no seu prompt para adicionar o conteúdo da transcrição 
+            do conteúdo do vídeo selecionado.</p>
+        </div>
+        <aside className="w-80"></aside>
+      </main>
     </div>
   )
 }
