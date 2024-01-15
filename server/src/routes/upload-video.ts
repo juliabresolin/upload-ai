e42f261eb7bbab07db2621bf99ffd28fd46699f2
@juliabresolin <julia.bresolin@jbq.global>
2024-01-15 16:51:33 -0300

    feat(server): create transcription route

--- server/src/routes/upload-video.ts
+++ server/src/routes/upload-video.ts
@@ -16,7 +16,7 @@ export async function uploadVideoRoute(app: FastifyInstance) {
     }
   })
 
-  app.post('/video', async (request, reply) => {
+  app.post('/videos', async (request, reply) => {
     const data = await request.file()
 
     if (!data) return reply.status(400).send({ error: 'Missing file input.'})
