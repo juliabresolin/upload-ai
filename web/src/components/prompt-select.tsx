0f761b70fff520afc934fd6a7940034a0f598b2c
@juliabresolin <julia.bresolin@jbq.global>
2024-01-16 11:18:56 -0300

    feat(web): get prompts

---
+++ web/src/components/prompt-select.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
+import { api } from "@/lib/axios";
+
+interface Prompt {
+  id: string
+  title: string
+  template: string
+}
+
+export function PromptSelect() {
+  const [prompts, setPrompts] = useState<Prompt[] | null>(null)
+
+  useEffect(() => {
+    api.get('/prompts').then(response => setPrompts(response.data))
+  }, [])
+
+  return (
+    <Select>
+      <SelectTrigger>
+        <SelectValue placeholder="Selecione um prompt" />
+      </SelectTrigger>
+      <SelectContent>
+        {prompts?.map(prompt => {
+          return (
+            <SelectItem value={prompt.title}>{prompt.title}</SelectItem>
+          )
+        })}
+      </SelectContent>
+    </Select>
+  )
+}
